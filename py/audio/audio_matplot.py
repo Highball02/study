2c03b7f0085279441003a4d8540b45be6dfc5ef9
Highball02 <tetu02052@outlook.jp>
2021-11-09 13:55:34 +0900

    commit

--- py/audio/audio_matplot.py
+++ py/audio/audio_matplot.py
@@ -3,20 +3,20 @@ import numpy
 import matplotlib.pyplot as plt
 
 chunk = 1024
-FORMAT = pyaudio.paInt16
+FORMAT = pyaudio.paInt16 #int型16bit
 
 CHANNELS = 1 #モノラル（2にするとステレオ）
 RATE = 44100 #サンプルレート（録音の音質）
-RECORD_SECONDS = 3 #録音時間
+RECORD_SECONDS = 5 #録音時間(s)
 
-p = pyaudio.PyAudio()
+p = pyaudio.PyAudio() #インスタンス生成
 
 stream = p.open(format = FORMAT,
                 channels = CHANNELS,
                 rate = RATE,
-                input = True,
-                frames_per_buffer = chunk)
-
+                input = True, 
+                frames_per_buffer = chunk)  # 録音開始（）内で設定
+#input_device_index = 1,  #録音デバイス設定
 #レコード開始
 print("Now Recording...")
 all = []
--- py/audio/mic_check.py
+++ py/audio/mic_check.py
@@ -2,3 +2,4 @@ import pyaudio
 p = pyaudio.PyAudio()
 for i in range(p.get_device_count()):
     print(p.get_device_info_by_index(i))
+p.terminate()
