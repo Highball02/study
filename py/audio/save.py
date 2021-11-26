560fa21cfd9342f1158c4eb767de4892a95306e8
Highball02 <tetu02052@outlook.jp>
2021-11-26 13:23:37 +0900

    こあだんぷ

--- py/audio/save.py
+++ py/audio/save.py
@@ -13,8 +13,8 @@ p = pyaudio.PyAudio()
 # 録音開始
 print("start")
 stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE,
-                input=True, frames_per_buffer=chunk,input_device_index=1)
-
+                input=True, frames_per_buffer=chunk,input_device_index=21)
+ 
 s_data = []
 for i in range(0, int(RATE/chunk*RECORD_SECONDS)):
     data = stream.read(chunk)
